98024196087d5514a538e8b796d4fb06728ef1b1
Martin Kolar <martin@igloonet.cz>
2019-01-23 22:59:55 +0100

    update after stream

--- src/scripts/firstScript.js
+++ src/scripts/firstScript.js
@@ -1,3 +1,56 @@
+import $ from 'jquery';
+
 export default function() {
-  console.log('Hello world!');
+  let jElement1 = $('#element1');
+  let jElement2 = $('.element');
+  let jElement3 = $('#element3');
+  let jElement5 = $('.element5');
+
+  let element1 = document.querySelector('#element1');
+  let element2 = document.querySelectorAll('.element');
+  let element3 = document.getElementById('element3');
+  let element4 = document.getElementsByClassName('element');
+  let element5 = document.querySelector('#element5');
+  let element6 = document.querySelectorAll('.egement');
+
+  // pridani classy
+  jElement1.addClass('test');
+  element1.classList.add('test');
+
+  // ma element classu
+  if (jElement1.hasClass('test')) {
+    console.log('jj jQuery');
+  }
+
+  if (element1.classList.contains('test')) {
+    console.log('jj vanilla');
+  }
+
+  // posluchaci
+  jElement2.click(function(event) {
+    console.log('jQuery: click on element');
+  });
+
+  [...element2].forEach((element) => {
+    element.addEventListener('click', function(event) {
+      console.log('vanilla: click on element');
+    });
+  });
+
+  // posluchac na neexistujicim elementu
+  jElement5.click(function(event) {
+    console.log('jQuery: click on element');
+  });
+
+  if (element5) {
+    element5.addEventListener('click', function(event) {
+      console.log('vanilla: click on element');
+    });
+  }
+
+  [...element6].forEach((element) => {
+    element.addEventListener('click', function(event) {
+      console.log('vanilla: click on element');
+    });
+  });
 }
--- src/scripts/secondScript.js
+++ src/scripts/secondScript.js
@@ -1,10 +1,30 @@
-export default function(options = {}) {
-  let _options = {
-    loadUrl: '?do=loadArticles',
-    loadFiltersUrl: '?do=loadFilters',
-    pageKey: 'pa',
-    page: 1
-  };
+// https://medium.com/@trombino.marco/you-might-not-need-jquery-a-2018-performance-case-study-aa6531d0b0c3
 
-  Object.assign(_options, options);
+import $ from 'jquery';
+
+export default function() {
+  // jquery
+  const startTimeJquery = new Date();
+  const elements = document.createDocumentFragment();
+  for(let i = 0; i < 10000; i++) {
+    $(elements).append($('<div />').addClass('test-div'));
+  }
+  $('#target-jquery').append(elements);
+
+  const endTimeJquery = new Date();
+
+  // vanilla js
+  const startTimeVanilla = new Date();
+  const c = document.createDocumentFragment();
+  for(let i = 0; i < 10000; i++) {
+     const e = document.createElement('div');
+     e.className = 'test-div';
+     c.appendChild(e);
+  }
+  document.getElementById('target-vanilla').appendChild(c);
+
+  const endTimeVanilla = new Date();
+
+  // log times
+  console.log(`jquery: ${endTimeJquery - startTimeJquery}ms`, `vanill: ${endTimeVanilla - startTimeVanilla}ms`)
 }
